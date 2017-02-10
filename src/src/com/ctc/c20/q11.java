64092ee325241b151d42e7f0d5c95e0b72bd915a
somu84 <someshwarchatterjee@gmail.com>
2017-02-09 21:32:03 -0500

    Q11 Fixed.

--- src/src/com/ctc/c20/q11.java
+++ src/src/com/ctc/c20/q11.java
@@ -6,22 +6,20 @@ public class q11 {
 		int i=0;
 		int j=0;
 		int maxSize = 0;
-		//int size = 0;
 		for(i=0; i<sqr.length; i++){
-			if(maxSize > sqr.length)
+			if(maxSize > sqr.length -i )
 				return maxSize;
 			for(j = 0; j<sqr.length; j++){
 				if(sqr[j][i].equals("B")){
 					int lColSize = getCol(sqr, j, i);
 					int uRowSize = getRow(sqr, j,i);
 					int minSize = (int)Math.min(lColSize, uRowSize);
-					int bRowSize = getRow(sqr,j+minSize,i);
+					int bRowSize = getRow(sqr,j+minSize-1,i);
 					minSize = (int)Math.min(bRowSize, minSize);
-					int rColSize = getCol(sqr,j,i+minSize);
+					int rColSize = getCol(sqr,j,i+minSize-1);
 					minSize = (int)Math.min(rColSize, minSize);
 					if(minSize > maxSize)
 						maxSize = minSize;
-					j+=(lColSize-1);
 				}
 			}
 			
@@ -31,7 +29,7 @@ public class q11 {
 	
 	public static int getCol(String[][] sqr, int row, int col){
 		int size = 0;
-		if(col >= sqr.length - 1)
+		if(col > sqr.length - 1)
 			return 1;
 		for(int i=row; i<sqr.length; i++){
 			if(sqr[i][col].equals("B")){
@@ -46,7 +44,7 @@ public class q11 {
 	
 	public static int getRow(String[][] sqr, int row, int col){
 		int size = 0;
-		if(row >= sqr.length -1)
+		if(row > sqr.length -1)
 			return 1;
 		for(int i=col; i<sqr.length; i++){
 			if(sqr[row][i].equals("B")){
@@ -59,6 +57,7 @@ public class q11 {
 		return size<1?1:size;
 	}
 	
+	//Driver
 	public static void main(String[] args){
 		String[][] str = {{"W","B","B","B","B"},
 						  {"B","W","B","B","W"},
